cc07163941099cd858982607d890e78fa2ebf16b
fonglh <fonglh@gmail.com>
2015-10-30 22:59:07 +0800

    Start writing a package for set1 challenge 1.
    Convert hex to base 64. Write test with given sample.
    Define a function and get it to return something first. Test currently
    fails.

---
+++ set1/hextobase64.go
@@ -0,0 +1,5 @@
+package hextobase64
+
+func Hextobase64(s string) string {
+	return s
+}
---
+++ set1/hextobase64_test.go
@@ -0,0 +1,19 @@
+package hextobase64
+
+import "testing"
+
+func TestHextobase64(t *testing.T) {
+	cases := []struct {
+		in, want string
+	}{
+		{"49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d",
+			"SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t"},
+	}
+
+	for _, c := range cases {
+		got := Hextobase64(c.in)
+		if got != c.want {
+			t.Errorf("Hextobase64(%q) == %q, want %q", c.in, got, c.want)
+		}
+	}
+}
