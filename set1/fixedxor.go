a0803890ef5d769cc000a4958ec9ed553a9fd4c1
fonglh <fonglh@gmail.com>
2015-11-07 16:48:54 +0800

    Move fixedxor implementation and test to set1 package

--- set1/fixedxor.go
+++
@@ -1,5 +0,0 @@
-package hextobase64
-
-func FixedXor(s1 string, s2 string) string {
-	return s1 + s2
-}
--- set1/set1.go
+++ set1/set1.go
@@ -10,3 +10,13 @@ func Hextobase64(s string) string {
 	base64_result := base64.StdEncoding.EncodeToString(raw_bytes)
 	return base64_result
 }
+
+func FixedXor(s1 string, s2 string) string {
+	s1_bytes, _ := hex.DecodeString(s1)
+	s2_bytes, _ := hex.DecodeString(s2)
+	var xor_output []byte
+	for i := 0; i < len(s1_bytes); i++ {
+		xor_output = append(xor_output, s1_bytes[i]^s2_bytes[i])
+	}
+	return hex.EncodeToString(xor_output)
+}
